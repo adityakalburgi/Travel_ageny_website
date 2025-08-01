74039ad2b8427c96fe0d9f66f76fb6252f0dadbd
Jiya Batra <jiyabatra2907@gmail.com>
2025-08-02 02:23:16 +0530

    Fixed About Page

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Header from './../Header/Header'
 import Routers from '../../router/Routers'
 import Footer from './../Footer/Footer'
+import "./Layout.css"
 
 const Layout = () => {
    return (
       <>
+      <div className='app-container'>
          <Header />
+         <main className='main-content'>
          <Routers />
+         </main>
          <Footer />      
+         </div>
       </>
    )
 }
