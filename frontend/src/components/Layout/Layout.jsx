3f66f14356b6633f9f5410aa0a4548ba208eb892
vaishnavi-1210 <poreddysaivasihnavi@gmail.com>
2025-08-03 11:56:23 +0530

    Resolved merge conflicts

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Header from './../Header/Header'
 import Routers from '../../router/Routers'
 import Footer from './../Footer/Footer'
